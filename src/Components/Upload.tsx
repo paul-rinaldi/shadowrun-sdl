e86b05635c7941463f2723589b2a4f9e367eee96
Seth Fenske <fenskesd@msoe.edu>
2020-10-04 23:44:23 -0500

    Prototype for uploading JSON

--- src/Components/Upload.tsx
+++ src/Components/Upload.tsx
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { InputFiles } from "typescript";
 import { IShadowRunState } from "../redux/store";
 import { showFileChooser, uploadPlayerJSON } from "../redux/uploadActions";
+import { ICharacter } from '../models/playerModels';
 
 type IUploadProps = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 const mapStateToProps = (state: IShadowRunState) => ({
@@ -39,9 +40,14 @@ class Upload extends React.Component<IUploadProps> {
                 if (!event.target?.result || typeof(event.target.result) !== "string")
                     throw new Error("Result unreadable");
                 let characterData = JSON.parse(event.target.result);
+                const characterObject = characterData as ICharacter;
+                if (this.isValidCharacter(characterObject)) {
+                    uploadPlayerJSON(characterData);
+                }
+
                 uploadPlayerJSON(characterData);
             } catch (e) {
-                alert("There was an error loading the file.");
+                alert("There was an error loading the file." + e);
             }
         }
         try {
@@ -52,6 +58,11 @@ class Upload extends React.Component<IUploadProps> {
             alert("There was an error reading the file.");
         }
     }
+
+    isValidCharacter = (character: ICharacter) => {
+        return true;
+    }
+
 }
 
 export default connect(
