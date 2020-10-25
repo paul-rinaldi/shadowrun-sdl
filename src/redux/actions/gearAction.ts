7cc2f2fd629e7470a659addf9c552da7ff0631cf
Seth Fenske <fenskesd@msoe.edu>
2020-10-25 13:45:37 -0500

    Gear action and reducers

--- src/redux/actions/gearAction.ts
+++ src/redux/actions/gearAction.ts
@@ -5,7 +5,13 @@ type SetMeleeAction = { type: 'SET_MELEE_ACTION', payload: Melee[] }
 type SetRangedAction = { type: 'SET_RANGED_ACTION', payload: Ranged[] }
 type SetGearAction = { type: 'SET_GEAR_ACTION', payload: Gear}
 
-export type GearAction = SetArmorAction | SetMeleeAction | SetRangedAction | SetGearAction;
+type AddMeleeAction = { type: 'ADD_MELEE_ACTION', payload: Melee}
+type AddRangedAction = { type: 'ADD_RANGED_ACTION', payload: Ranged}
+type AddArmorAction = { type: 'ADD_ARMOR_ACTION', payload: Armor}
+
+export type GearAction = SetArmorAction | SetMeleeAction | SetRangedAction | SetGearAction |
+    AddMeleeAction | AddArmorAction | AddRangedAction;
+
 
 export const setArmor = (armor: Armor[]): SetArmorAction => ({
     type: 'SET_ARMOR_ACTION',
@@ -27,3 +33,17 @@ export const setGear = (gear: Gear): SetGearAction => ({
     payload: gear
 });
 
+export const addMelee = (melee: Melee): AddMeleeAction => ({
+    type: 'ADD_MELEE_ACTION',
+    payload: melee
+});
+
+export const addRaned = (ranged: Ranged): AddRangedAction => ({
+    type: 'ADD_RANGED_ACTION',
+    payload: ranged
+});
+
+export const addArmor = (armor: Armor): AddArmorAction => ({
+    type: 'ADD_ARMOR_ACTION',
+    payload: armor
+});
--- src/redux/reducers/gearReducer.ts
+++ src/redux/reducers/gearReducer.ts
@@ -6,8 +6,11 @@ export const gearReducer = (action: GearAction, state: Gear = initialState.gear)
     switch (action.type) {
         case 'SET_ARMOR_ACTION': return { ...state, armor: action.payload };
         case 'SET_MELEE_ACTION': return { ...state, melee: action.payload };
-        case 'SET_RANGED_ACTION': return { ...state, ranged: action.payload }
+        case 'SET_RANGED_ACTION': return { ...state, ranged: action.payload };
         case 'SET_GEAR_ACTION': return action.payload;
+        case 'ADD_ARMOR_ACTION': return {...state, armor: [...state.armor, action.payload]};
+        case 'ADD_MELEE_ACTION': return {...state, melee: [...state.melee, action.payload]};
+        case 'ADD_RANGED_ACTION': return {...state, ranged: [...state.ranged, action.payload]};
         default: return state;
     }
 }
