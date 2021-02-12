49e19c499cb96bbc61e9df477149dd0696db360f
Kam <mitchellk@msoe.edu>
2021-02-11 18:41:39 -0600

    Test is finally doing something.... its atleast looking at the redux files.

--- src/Tests/gear.test.tsx
+++ src/Tests/gear.test.tsx
@@ -1,12 +1,19 @@
 // gear.test.js
 import React from 'react';
-import {configure, mount, shallow} from 'enzyme';        //Enzyme makes testing react components easier
+import {configure, mount, render, shallow} from 'enzyme';        //Enzyme makes testing react components easier
 import Adapter from 'enzyme-adapter-react-16';
 import {Provider} from 'react-redux';
-//import configureStore from 'redux-mock-store';
+import configureStore from 'redux-mock-store';
 //import renderer from 'react-test-renderer';
 //Actual component to be tested
 import {GearPage} from "../Components/Gear";
+import {addArmor, addRanged, remArmor} from "../redux/actions/gearAction";
+import {combineReducers, createStore} from "redux";
+import {makeLog} from "../redux/actions/logActions";
+import {adjustNuyen} from "../redux/actions/nuyenActions";
+import {store} from "../redux/store";
+import {logReducer} from "../redux/reducers/logReducer";
+import {nuyenReducer} from "../redux/reducers/nuyenReducer";
 
 //Use the filesystem to load the test file
 const fs = require('fs');
@@ -15,7 +22,7 @@ const fs = require('fs');
 configure({adapter: new Adapter()});
 
 //setup mock store
-//const mockStore = configureStore();
+const mockStore = configureStore();
 
 //set up mock dispatch
 const armor = {
@@ -27,75 +34,60 @@ const armor = {
     equiped: true
 };
 
-const mockDispatchfn = jest.fn(() => new Promise(resolve => resolve({
-    armor
-})));
 
 describe('Adding gear from app', () => {
-    let wrapper : any // this needs to be any and cannot be "const" because const does not allow any class component to work
-    const testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json'));
-    window.prompt = jest.fn(() => "2");
-    let logMock = {
-        adjustment: 2,
-        reason: "just cause",
-        reasonType: "idk",
-        time: 11
-    }
-    let mockMakeLog = jest.fn(()=> {
-        return logMock;
-    });
-    let mockAdjustNuyen = jest.fn(()=> 4);
-    let mockCharacter = jest.fn();
-    let mockArmor = jest.fn(()=> {return armor});
-    wrapper = shallow(<GearPage character={testLuigi} adjustNuyen={mockAdjustNuyen} makeLog={mockMakeLog} addArmor={mockArmor}/>);
+    let wrapper: any // this needs to be any and cannot be "const" because const does not allow any class component to work
+    let testLuigi: any
+    let store = createStore(
+        combineReducers({logReducer, nuyenReducer, addArmor})
+    );
 
-    it('Add gear', () => {
-        //Arrange
-        // wrapper = shallow(<GearPage/>); //Shallow render of the Gear component
-        // const initialState = {};
-        // const store = mockStore(initialState);
-
-
-        //Load the test character from the file
-        // const testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json'));
-        wrapper.setState(testLuigi);
-
-        const armor = {
-            name: "name",
-            rating: 20,
-            capacity: 1,
-            availability: 1,
-            cost: 100,
-            equiped: true
-        };
-
-        //Act
-        // store.dispatch(component.instance().removeGear("armor", 0));
-        //console.log(wrapper.instance().state.gear.armor);
-        wrapper.instance().addGearArmor("armor", 0);
-        console.log(wrapper.instance().state.gear.armor);
-        // //Assert
-        // const actions = store.getActions();
-        // const expectedPayload = {type: 'REM_ARMOR_ACTION', payload: 0};
-        expect(wrapper.instance().state.gear.armor[1].name).toBe("name")
-        // expect(actions).toEqual([expectedPayload]);
+    testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json'));
+    window.prompt = jest.fn(() => {
+        return "Jacket"
     });
 
-    // it('Remove gear', () => {
-    //     //Arrange
-    //     wrapper = shallow(<GearPage/>); //Shallow render of the App component
-    //
-    //     //Load the test character from the file
-    //     const testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json'));
-    //     wrapper.setState(testLuigi);
-    //
-    //     //Act
-    //     wrapper.instance().updateRemGear(1, "armor");
-    //
-    //     //Assert
-    //     expect(wrapper.instance().state.gear.armor.length).toBe(1)
-    // });
+    wrapper = mount(<Provider store={store}>
+        <GearPage character={testLuigi}/>
+    </Provider>);
+
+
+
+it('Add gear', () => {
+    //Arrange
+    wrapper.setState(testLuigi);
+    store.dispatch(makeLog(1, "idk", "idk", new Date()));
+    store.dispatch(adjustNuyen(3))
+    store.dispatch(addArmor(armor));
+    //Act
+    // store.dispatch(component.instance().removeGear("armor", 0));
+    wrapper.instance().addGearArmor();
+
+
+    //Assert
+    expect(wrapper.instance().state.gear.armor[1].name).toBe("Jacket")
 });
+//
+// it('Remove gear', () => {
+//     //Arrange
+//     const testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json'));
+//
+//     // wrapper = shallow(<GearPage character={testLuigi} adjustNuyen={mockAdjustNuyen} makeLog={mockMakeLog} remArmor={remArmor}/>); //Shallow render of the App component
+//     window.prompt = jest.fn(() => {
+//         return '2'
+//     });
+//     //Load the test character from the file
+//
+//     //wrapper.setState(testLuigi);
+//
+//     //Act
+//     wrapper.instance().removeGear("armor", 1);
+//
+//     //Assert
+//     expect(wrapper.instance().state.gear.armor.length).toBe(1)
+// });
+})
+;
 
 
 // describe('Equip and unequip gear', () => {
