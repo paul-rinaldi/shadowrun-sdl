00832988ec113a0230b82d9f5da42545b041f20f
Kam <mitchellk@msoe.edu>
2021-02-13 16:57:21 -0600

    Worked with Nick to format and mock necessary items.

--- src/Tests/gear.test.tsx
+++ src/Tests/gear.test.tsx
@@ -31,51 +31,67 @@ const armor = {
 
 
 describe('Adding gear from app', () => {
+
     let wrapper: any // this needs to be any and cannot be "const" because const does not allow any class component to work
-    let testLuigi: any
+    let testLuigi: any // is the character of the test
+    let instance: any // for the wrapper.instance()
+    let log: any; // will be used for the log object to pass to log reducer
+    let date: any; // will be used for the date of the log object
+
     let store = createStore(
-        combineReducers({logReducer, nuyenReducer, gearReducer})
+        combineReducers({logReducer, nuyenReducer, gearReducer}) // the reducers of the current function
     );
 
-    testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json'));
+    testLuigi = JSON.parse(fs.readFileSync('src/Tests/TestLuigi.json')); //converts the test Luigi to a json to be used in the test
 
-    let mockLog = jest.fn();
-    let mockNuyen = jest.fn();
-    let mockAddArmor = jest.fn();
+    beforeEach(()=> {
+        date = new Date();
 
-    wrapper = shallow(<GearPage store={store} character={testLuigi} makeLog={mockLog} adjustNuyen={mockNuyen}
-                                addArmor={mockAddArmor}/>);
-    wrapper.makeLog = jest.fn(()=> {
-        console.log("did this work?");
-    })
-    let instance = wrapper.instance();
-    wrapper.setState(testLuigi);
-    //let date: any;
-    let log: any;
-   // wrapper.instance().addGearArmor = jest.fn(() => {
-        let date = new Date();
-        log = {
-            adjustment: 1, reason: "idk", reasonType: "idk", time: new Date()
+        let props = { // props for the GearPage
+            character: testLuigi,
+            makeLog: jest.fn(),
+            addArmor: jest.fn(),
+            adjustNuyen: jest.fn(),
+            addRanged: jest.fn(),
+            addMelee: jest.fn(),
+            remArmor: jest.fn(),
+            remMelee: jest.fn(),
+            remRanged: jest.fn(),
+            toggleEquip: jest.fn(),
+            setGear: jest.fn()
         };
-        store.dispatch(makeLog(1, "idk", "idk", date));
-        store.dispatch(adjustNuyen(3))
-        store.dispatch(addArmor(armor));
-        instance.state = store.getState();
-   // });
+
+        log = {
+            adjustment: 1,
+            reason: "idk",
+            reasonType: "idk",
+            time: new Date()
+        };
+
+        wrapper = shallow(<GearPage {...props}/>);
+        instance = wrapper.instance();
+        wrapper.setState(testLuigi); // set the state of the wrapper to testLuigi
+    });
+
 
     it('Adds gear armor', () => {
         //Arrange
-        let increment = 0; // used for the different window prompts
-        window.prompt = jest.fn(()=> increment++ === 1? "Jacket": "3")
+        let increment = 0; // used for the different window prompts of addGearArmor() method
+        store.dispatch(makeLog(1, "idk", "idk", date)); //send the object to the reducer to calculate the new state
+        store.dispatch(adjustNuyen(-3)) // same as above
+        store.dispatch(addArmor(armor)); //same as above
+        instance.state = store.getState(); // get the new state from the store after all actions are computed
+
+        window.prompt = jest.fn(()=> increment++ === 1? "Jacket": "3") // will gives the following values to the prompt. The prompts asks for numbers after the initial one so change from word to number to parseInt
 
         //Act
-        instance.addGearArmor();
+        instance.addGearArmor(); //call the method to be done
 
         //Assert
-        expect(instance.state.gearReducer.armor[2].name).toBe("Jacket");
-        expect(instance.state.gearReducer.armor.length).toBe(3);
-        expect(instance.state.nuyenReducer).toBe(10003);
-        expect(instance.state.logReducer[0]).toEqual(log);
+        expect(instance.state.gearReducer.armor[2].name).toBe("Jacket"); // see if the new state is correct
+        expect(instance.state.gearReducer.armor.length).toBe(3); // same as above
+        expect(instance.state.nuyenReducer).toBe(9997); // same as above
+        expect(instance.state.logReducer[0]).toEqual(log); // same as above
     });
 //
 // it('Remove gear', () => {
